f46a3f009049835b2ec7adca3d99d914690c6441
Nofal <nofalaamir@gmail.com>
2018-10-22 23:38:00 -0700

    prelab6 - super not working

---
+++ lab6/prelab/gd.c
@@ -0,0 +1,22 @@
+/********* gd.c code ***************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <ext2fs/ext2_fs.h>
+
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+
+// define shorter TYPES, save typing efforts
+typedef struct ext2_group_desc  GD;
+typedef struct ext2_super_block SUPER;
+typedef struct ext2_inode       INODE;
+typedef struct ext2_dir_entry_2 DIR;    // need this for new version of e2fs
+
+GD    *gp;
+SUPER *sp;
+INODE *ip;
+DIR   *dp; 
+
+#define BLKSIZE 1024
